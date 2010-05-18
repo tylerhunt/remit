ed316e79824eb0dbe9c1c0069223f91935d63f87
Jeremy Green <jagthedrummer@gmail.com>
2010-05-19 06:12:18 +0800

    Updated get_account_activity integration test to match the new data model.

--- spec/integrations/get_account_activity_spec.rb
+++ spec/integrations/get_account_activity_spec.rb
@@ -9,22 +9,26 @@ describe 'a GetAccountActivity call' do
     @response = remit.get_account_activity(request)
   end
 
+  it "has results" do
+    @response.get_account_activity_result.should_not be_nil
+  end
+
   it 'should have a collection of transactions' do
-    @response.should have_at_least(1).transactions
+    @response.get_account_activity_result.should have_at_least(1).transactions
   end
 
   it 'should have a transaction with all of its values set' do
-    transaction = @response.transactions.first
+    transaction = @response.get_account_activity_result.transactions.first
     transaction.caller_name.should_not be_empty
     #transaction.caller_token_id.should_not be_empty
     transaction.caller_transaction_date.should_not be_nil
     transaction.date_completed.should_not be_nil
     transaction.date_received.should_not be_nil
     transaction.error_code.should eql("")
-    transaction.error_detail.should eql("")
-    transaction.error_message.should eql("")
+    #transaction.error_detail.should eql("")
+    #transaction.error_message.should eql("")
     transaction.fees.should_not be_nil
-    transaction.fpsoperation.should_not be_empty
+    transaction.fps_operation.should_not be_empty
     transaction.recipient_name.should_not be_empty
     transaction.sender_name.should_not be_empty
     transaction.sender_token_id.should_not be_empty
